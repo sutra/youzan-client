b08968bd808fb4742d965a8c5dad10db78d3dcf3
Joe Zhou <zhoushuqun@gmail.com>
2016-06-23 11:33:55 +0800

    Error 40003 when system time zone is not Asia/Shanghai.

--- youzan-client-service/src/main/java/com/kdt/api/KdtApiClient.java
+++ youzan-client-service/src/main/java/com/kdt/api/KdtApiClient.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -25,7 +26,7 @@ public class KdtApiClient {
     private static final String format = "json";
 
     private static final String signMethod = "md5";
-    
+
     private static final String DefaultUserAgent = "KdtApiSdk Client v0.1";
 
     private String appId;
@@ -35,29 +36,29 @@ public class KdtApiClient {
         if ("".equals(appId) || "".equals(appSecret)){
             throw new Exception("appId 和 appSecret 不能为空");
         }
-        
+
         this.appId = appId;
         this.appSecret = appSecret;
     }
-    
+
     public HttpResponse get(String method, HashMap<String,String> parames) throws Exception{
         String url = apiEntry + getParamStr(method, parames);
-        
+
         HttpClient client = new DefaultHttpClient();
 		HttpGet request = new HttpGet(url);
 		request.addHeader("User-Agent", DefaultUserAgent);
- 
+
 		HttpResponse response = client.execute(request);
 		return response;
     }
-    
+
     public HttpResponse post(String method, HashMap<String, String> parames, List<String> filePaths, String fileKey) throws Exception{
     	String url = apiEntry + getParamStr(method, parames);
-    	
+
     	HttpClient client = new DefaultHttpClient();
     	HttpPost httppost = new HttpPost(url);
     	httppost.addHeader("User-Agent", DefaultUserAgent);
-    	
+
     	if(null != filePaths && filePaths.size() > 0 && null != fileKey && !"".equals(fileKey)){
 	    	MultipartEntity mpEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
 	    	for(int i = 0; i < filePaths.size(); i++){
@@ -65,16 +66,16 @@ public class KdtApiClient {
 	    		ContentBody cbFile = new FileBody(file);
 	            mpEntity.addPart(fileKey, cbFile);
 	    	}
-	        
+
 	        httppost.setEntity(mpEntity);
     	}
-    	
+
         System.out.println("executing request " + httppost.getRequestLine());
         HttpResponse response = client.execute(httppost);
-        
+
         return response;
     }
-    
+
     public String getParamStr(String method, HashMap<String, String> parames){
         String str = "";
         try {
@@ -90,7 +91,7 @@ public class KdtApiClient {
 
         return str;
     }
-    
+
     private String buildParamStr(HashMap<String, String> param){
         String paramStr = "";
         Object[] keyArray = param.keySet().toArray();
@@ -115,10 +116,10 @@ public class KdtApiClient {
 			if(commonParams.containsKey(key)){
 				throw new Exception("参数名冲突");
 			}
-			
+
 			commonParams.put(key, parames.get(key));
 		}
-        
+
         commonParams.put(KdtApiProtocol.SIGN_KEY, KdtApiProtocol.sign(appSecret, commonParams));
         return commonParams;
     }
@@ -127,7 +128,9 @@ public class KdtApiClient {
        HashMap<String, String> parames = new HashMap<String, String>();
         parames.put(KdtApiProtocol.APP_ID_KEY, appId);
         parames.put(KdtApiProtocol.METHOD_KEY, method);
-        parames.put(KdtApiProtocol.TIMESTAMP_KEY, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+        parames.put(KdtApiProtocol.TIMESTAMP_KEY, sdf.format(new Date()));
         parames.put(KdtApiProtocol.FORMAT_KEY, format);
         parames.put(KdtApiProtocol.SIGN_METHOD_KEY, signMethod);
         parames.put(KdtApiProtocol.VERSION_KEY, Version);
